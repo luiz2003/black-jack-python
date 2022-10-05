14d4208eb6e12d3cab62610534b6e1a3b566ea76
Luiz Valverde <luizcastroval321@gmail.com>
2022-10-05 10:33:16 -0300

    add sprites to sprite folder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyglet
+from pathlib import Path
 
 window = pyglet.window.Window(caption='Blackjack')
 
@@ -6,49 +7,49 @@ batch = pyglet.graphics.Batch()
 background = pyglet.graphics.OrderedGroup(0)
 cards = pyglet.graphics.OrderedGroup(1)
 
-icon = pyglet.image.load('blackjack_icon.png')
+icon = pyglet.image.load(Path('sprites/blackjack_icon.png'))
 window.set_icon(icon)
 
-background_image = pyglet.image.load('blackjack_background.jpg')
+background_image = pyglet.image.load(Path('sprites/blackjack_background.jpg'))
 background = pyglet.sprite.Sprite(background_image, batch=batch, group=background)
 
-AEspadas_image = pyglet.image.load('AEspadas.png')
+AEspadas_image = pyglet.image.load(Path('sprites/AEspadas.png'))
 AEspadas = pyglet.sprite.Sprite(AEspadas_image, batch=batch, group=cards, x=10, y=200)
 
-DoisEspadas_image = pyglet.image.load('2Espadas.png')
+DoisEspadas_image = pyglet.image.load(Path('sprites/2Espadas.png'))
 DoisEspadas = pyglet.sprite.Sprite(DoisEspadas_image, batch=batch, group=cards, x=50, y=200)
 
-TresEspadas_image = pyglet.image.load('3Espadas.png')
+TresEspadas_image = pyglet.image.load(Path('sprites/3Espadas.png'))
 TresEspadas = pyglet.sprite.Sprite(TresEspadas_image, batch=batch, group=cards, x=90, y=200)
 
-QuatroEspadas_image = pyglet.image.load('4Espadas.png')
+QuatroEspadas_image = pyglet.image.load(Path('sprites/4Espadas.png'))
 QuatroEspadas = pyglet.sprite.Sprite(QuatroEspadas_image, batch=batch, group=cards, x=130, y=200)
 
-CincoEspadas_image = pyglet.image.load('5Espadas.png')
+CincoEspadas_image = pyglet.image.load(Path('sprites/5Espadas.png'))
 CincoEspadas = pyglet.sprite.Sprite(CincoEspadas_image, batch=batch, group=cards, x=170, y=200)
 
-SeisEspadas_image = pyglet.image.load('6Espadas.png')
+SeisEspadas_image = pyglet.image.load(Path('sprites/6Espadas.png'))
 SeisEspadas = pyglet.sprite.Sprite(SeisEspadas_image, batch=batch, group=cards, x=210, y=200)
 
-SeteEspadas_image = pyglet.image.load('7Espadas.png')
+SeteEspadas_image = pyglet.image.load(Path('sprites/7Espadas.png'))
 SeteEspadas = pyglet.sprite.Sprite(SeteEspadas_image, batch=batch, group=cards, x=250, y=200)
 
-OitoEspadas_image = pyglet.image.load('8Espadas.png')
+OitoEspadas_image = pyglet.image.load(Path('sprites/8Espadas.png'))
 OitoEspadas = pyglet.sprite.Sprite(OitoEspadas_image, batch=batch, group=cards, x=290, y=200)
 
-NoveEspadas_image = pyglet.image.load('9Espadas.png')
+NoveEspadas_image = pyglet.image.load(Path('sprites/9Espadas.png'))
 NoveEspadas = pyglet.sprite.Sprite(NoveEspadas_image, batch=batch, group=cards, x=330, y=200)
 
-DezEspadas_image = pyglet.image.load('10Espadas.png')
+DezEspadas_image = pyglet.image.load(Path('sprites/10Espadas.png'))
 DezEspadas = pyglet.sprite.Sprite(DezEspadas_image, batch=batch, group=cards, x=370, y=200)
 
-ValeteEspadas_image = pyglet.image.load('ValeteEspadas.png')
+ValeteEspadas_image = pyglet.image.load(Path('sprites/ValeteEspadas.png'))
 ValeteEspadas = pyglet.sprite.Sprite(ValeteEspadas_image, batch=batch, group=cards, x=410, y=200)
 
-RainhaEspadas_image = pyglet.image.load('RainhaEspadas.png')
+RainhaEspadas_image = pyglet.image.load(Path('sprites/RainhaEspadas.png'))
 RainhaEspadas = pyglet.sprite.Sprite(RainhaEspadas_image, batch=batch, group=cards, x=450, y=200)
 
-ReiEspadas_image = pyglet.image.load('ReiEspadas.png')
+ReiEspadas_image = pyglet.image.load(Path('sprites/ReiEspadas.png'))
 ReiEspadas = pyglet.sprite.Sprite(ReiEspadas_image, batch=batch, group=cards, x=490, y=200)
 
 @window.event
