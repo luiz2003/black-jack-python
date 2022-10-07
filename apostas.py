005131d8d37837de69e065dc6cd7f4ea9fc68a8f
marmeladov69 <115194380+marmeladov69@users.noreply.github.com>
2022-10-06 21:36:56 -0300

    Add files via upload

---
+++ apostas.py
@@ -0,0 +1,41 @@
+'''
+--- Anotações ---
+
+  No Blackjack, você pode apostar qualquer valor x tq 0 < x <= TETO, em que TETO, no nosso jogo:
+-> dobra a cada 3 partidas
+-> reseta para TETO_INICIAL cada vez que o jogador perde
+
+  Sobre contador_partidas no código abaixo:
+-> a cada vez que jogador ganha uma partida, contador_partidas += 1
+-> a cada vez que jogador perde uma partida, contador_partidas = 0
+-> lógica deve ser implementada em main (farei em breve)
+'''
+
+class Aposta:
+    TETO_INICIAL = 1500
+    
+    def __init__(self, valor, teto=TETO_INICIAL):
+        self._teto = teto
+        
+        if valor > 0 and valor <= teto:
+            self.valor = valor
+        else:
+            print("Aposta inválida.")
+    
+    def get_teto(self):
+        return self._teto
+    
+    def aumentar_teto(self):
+        if contador_partidas == 3:
+            self._teto = 2 * self._teto 
+    
+    def resetar_teto(self):
+        if contador_partidas == 0:
+            self._teto = teto_inicial
+            
+    def nova_aposta(self, novo_valor):
+        if novo_valor > 0 and novo_valor <= teto:
+            self.valor = novo_valor
+        else:
+            print("Aposta inválida.")
+    
