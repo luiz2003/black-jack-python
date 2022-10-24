28763bd3e69f825d9c9cf036620ec6870a469863
Qualloss <112492368+Qualloss@users.noreply.github.com>
2022-10-24 14:23:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
 import pyglet
-from pathlib import Path
-from src.game.game import Game
-from src.botoes.botoes_main import BotaoComprar, BotaoParar
+from menuu import MeuMenu
 
-batch = pyglet.graphics.Batch()
-cards = pyglet.graphics.OrderedGroup(1)
-background_group = pyglet.graphics.OrderedGroup(0)
-
-window = pyglet.window.Window(caption='Blackjack')
-
-icon = pyglet.image.load(Path('sprites/blackjack_icon.png'))
-window.set_icon(icon)
-
-background_image = pyglet.image.load(Path('sprites/blackjack_background.jpg'))
-background = pyglet.sprite.Sprite(background_image, group=background_group)
-
-game = Game(
-    batch = batch,
-    group = cards
-)
-
-
-@window.event
-def on_mouse_press(x,y, button, modifiers):
-    game.botao_comprar.clica(x,y)
-    game.botao_parar.clica(x,y)
-
-@window.event
-def on_draw():
-    window.clear()
-    background.draw()
-    
-    if game.is_over:
-        game.draw_result()
-        print(game.result)
-          
-    else:
-        batch.draw()
-        game.botao_comprar.draw()
-        game.botao_parar.draw()
+menu = MeuMenu()
 
 pyglet.app.run()
