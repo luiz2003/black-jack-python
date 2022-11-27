ff90e871cff1cf00cbb6fbb6dd4850d95e7acdae
marmeladov69 <115194380+marmeladov69@users.noreply.github.com>
2022-11-26 21:16:56 -0300

    Update apostas.py

--- apostas.py
+++ apostas.py
@@ -3,12 +3,7 @@
 
   No Blackjack, você pode apostar qualquer valor x tq 0 < x <= TETO, em que TETO, no nosso jogo:
 -> dobra a cada partida vencida
--> reseta para TETO_INICIAL cada vez que o jogador perde
-
-  Sobre contador_partidas no código abaixo:
--> a cada vez que jogador ganha uma partida, contador_partidas += 1
--> a cada vez que jogador perde uma partida, contador_partidas = 0
--> lógica deve ser implementada em test
+-> reseta para TETO_INICIAL cada vez que o jogador perde ou empata
 '''
 
 class Aposta:
@@ -25,17 +20,22 @@ class Aposta:
     def get_teto(self):
         return self._teto
     
-    def aumentar_teto(self):
-        if contador_partidas != 0: #enquanto jogador não perder...
+    def aumentar_teto(self, indicador_partidas):
+        if indicador_partidas == "player_valormaior" or indicador_partidas == "dealer_estourou": #jogador ganha
             self._teto = 2 * self._teto 
     
-    def resetar_teto(self):
-        if contador_partidas == 0:
+    def resetar_teto(self, indicador_partidas):
+        if indicador_partidas == "player_estourou" or indicador_partidas == "dealer_valormaior":
+            self._teto = teto_inicial
+    
+    def resetar_aposta(self, indicador_partidas):
+        if indicador_partidas == "player_estourou" or indicador_partidas == "dealer_valormaior":
             self._teto = teto_inicial
             
     def nova_aposta(self, novo_valor):
         if novo_valor > 0 and novo_valor <= teto:
             self.valor = novo_valor
         else:
-            print("Aposta inválida.")
-    
+            print("Aposta inválida."
+   
+                 
