6409f07b9dcd81ea2bd4a2267eeb8a6631e0f23e
marmeladov69 <115194380+marmeladov69@users.noreply.github.com>
2022-11-27 00:31:23 -0300

    Update apostas.py

--- src/apostas/apostas.py
+++ src/apostas/apostas.py
@@ -7,10 +7,59 @@ import pyglet
 from pathlib import Path
 from ..game import game
 from ..player import player
+from src.botoes.botao_main import Widget
 
-class Aposta:   
-    def __init__(self)->None:
-            self.valor = 1500
+class Input(Widget):
+    numeros = ["0","1","2","3","4","5","6","7","8","9"]
+    
+    def __init__(self, x, y, width, height, texto=""):
+        super().__init__(x, y, width, height,texto)
+        self.selecionado = False
+        self.texto = texto
+        self.moldura = pyglet.shapes.BorderedRectangle(x, y, width, height, color=(128,0,0), border_color=(255, 255, 255))
+        self.linha = pyglet.shapes.Line(x, y + 5, x + width, y + 5)
+        self.label = pyglet.text.Label(self.texto,
+                font_name='Arial',
+                font_size=32,
+                anchor_x='center', anchor_y='center',
+                x = self.x + self.width // 2,
+                y = self.y + self.height // 2)
+    def draw(self):
+        if self.selecionado:
+            self.moldura.draw()
+        self.linha.draw()
+        self.label.draw()
+    def clica(self, x, y):
+        self.selecionado = False
+        super().clica(x, y)
+    def on_click(self):
+        self.selecionado = True
+        self.label.text = "0"
+        
+    def digita(self, symbol):
+        if not self.selecionado:
+            return 
+        if symbol == "BACKSPACE":
+            self.label.text = self.label.text[0:-1]
+        if symbol in numeros:
+            self.label.text += symbol
+        else:
+            raise ValueError("Digite apenas números.")
+
+    def __repr__(self):
+        return f"Input '{self.texto}'" 
+
+class Aposta(Input):   
+    def __init__(self):
+            self.valor = 0 #valor começa em 0
+            super().__init__(x, y, width, height, texto=f'Valor da aposta: {self.valor}')
+            
+            
+    nova_aposta(self): 
+        super().digita()
+        self.valor = super().__repr__()
+        self.texto = f'Valor da aposta: {self.valor}'
+        
    
     def resultado(self, indicador_partidas, indicador_blackjack):
         if indicador_partidas == "player_estourou" or indicador_partidas == "dealer_valormaior": #jogador perde
